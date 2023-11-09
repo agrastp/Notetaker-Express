d26f0c35bb1aa075befa9811b3081c7d0b3c8fe7
AgrastP <agrastp@yahoo.com>
2023-11-09 12:02:35 -0500

    changed routes files

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -4,5 +4,35 @@ const router = require('express').Router();
 const uniqid = require('uniqid');
 
 // GET /api/notes should read the json db.json file and return saved notes
+router.get('/api/notes', (req, res) => {
+    fs.readFile(db, (err, data) => {
+        if (err) throw err;
+        console.log(JSON.parse(data));
+        res.send(data)
+    })
+});
 // POST /api/notes should revieve a new note to save on the req.body, add it to db.json and return to the client
 //Each note will need a unique id using the npm package uniqid
+router.post('/api/notes', (req, res) => {
+    let newNote = {
+        id: uniqid(),
+        title: req.body.title,
+        text: req.body.text
+    }
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        let newData = JSON.parse(data);
+
+        newData.push(newNote);
+        console.log(newData)
+
+        fs.writeFile('./db/db.json', JSON.stringify(newData), (err) => {
+            if (err) throw err;
+
+            res.send('note has been added')
+        });
+    });
+});
+
+
+module.exports = router;
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const path = require('path');
 
 router.get("/notes", (req, res) =>{
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+    console.log('logging into notes');
 });
 
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+    console.log('logging into index');
 });
 
 module.exports = router;
