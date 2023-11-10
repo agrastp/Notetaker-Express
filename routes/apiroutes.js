fe2f1db1b2c3c54b152a410b41663d6d8dba6f02
AgrastP <agrastp@yahoo.com>
2023-11-10 15:20:05 -0500

    add post note

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,39 +1,60 @@
 // const fs = require('fs');
 const router = require('express').Router();
-// const uniqid = require('uniqid');
 
-router.get('/api/notes', (req, res) => {
-    fs.readFileSync('./db/db.json', 'utf8').then((data) => res.json(JSON.parse(data)));
+const {
+    readFromFile,
+    readAndAppend,
+  } = require('../helpers/fsUtils');
+const uniqid = require('uniqid');
+
+// GET /api/notes should read the json db.json file and return saved notes
+
+router.get('/notes', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
 
 
 
-// GET /api/notes should read the json db.json file and return saved notes
-// router.get('/api/notes', (req, res) => {
-//     res.json(dbData);
-// });
-  
 // POST /api/notes should revieve a new note to save on the req.body, add it to db.json and return to the client
 //Each note will need a unique id using the npm package uniqid
-// router.post('/api/notes', (req, res) => {
-//     console.info(`${req.method} request recieved to add a note`);
 
-//     const { id, title, text } = req.body;
+  router.post('/notes', (req, res) => {
+    console.log(req.body);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        note_id: uniqid(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Note added successfully`);
+    } else {
+      res.error('Error in adding note');
+    }
+  });
 
-//     if (id && title && text) {
-//         const newNote = {
-//             id: uniqid(), 
-//             title, 
-//             text
-//         };
+// DELETE Route for a specific tip
+router.delete('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        // Make a new array of all tips except the one with the ID provided in the URL
+        const result = json.filter((title) => title.note_id !== noteId);
+  
+        // Save that array to the filesystem
+        writeToFile('./db/db.json', result);
+  
+        // Respond to the DELETE request
+        res.json(`Item ${noteId} has been deleted 🗑️`);
+      });
+  });
+  
 
-//         const response = {
-//             status: 'success', 
-//             body: newNote,
-//         };
-//     };
-// });
-        
-    
-    
+
+   
 module.exports = router;
